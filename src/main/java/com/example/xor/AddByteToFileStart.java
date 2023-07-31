4135c9e658991b5c62157af53d4d20a9adbb1d88
hss01248 <hss012489@gmail.com>
2023-07-31 17:35:05 +0800

    文件夹操作

--- src/main/java/com/example/xor/AddByteToFileStart.java
+++ src/main/java/com/example/xor/AddByteToFileStart.java
@@ -1,5 +1,7 @@
 package com.example.xor;
 
+import java.io.File;
+
 /**
  * https://zhuanlan.zhihu.com/p/571208394
  * https://www.cnblogs.com/zhaijiahui/p/9643599.html
@@ -7,9 +9,11 @@ package com.example.xor;
  */
 public class AddByteToFileStart {
     public static void main(String[] args) {
-        String filePath = "/Users/hss/1download/flipper使用2021-06-24 19.17.43.mov";
+       /* String filePath = "/Users/hss/1download/flipper使用2021-06-24 19.17.43.mov";
         String path2 = AddByteUtil.addByte(filePath);
-        AddByteUtil.createTmpOriginalFile(null,path2);
+        AddByteUtil.createTmpOriginalFile(null,path2);*/
+
+        AddByteUtil.hideDirAndInnerFiles(new File("/Users/hss/1download/"));
     }
 }
 
